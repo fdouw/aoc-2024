0973d0c57db1ea494aec8496e2c5ac6ccea2f917
Floris Douw <fdouw@hotmail.com>
2024-12-22 13:57:17 +0100

    Minor optimisations for day 22

--- src/solutions/day22.rs
+++ src/solutions/day22.rs
@@ -5,9 +5,11 @@ pub fn solve(input: String, _verbose: bool) -> (String, String) {
 
     let mut results = Vec::new();
     let mut total_bananas = HashMap::new();
+    let mut seen = HashSet::new();
+    let mut prices = Vec::new();
     for num in initial_numbers {
         let mut n = num;
-        let mut prices = Vec::new();
+        prices.clear();
         prices.push(n % 10);
         for _ in 0..2000 {
             n = (n ^ (n << 6)) & 0x00FFFFFF;
@@ -18,9 +20,10 @@ pub fn solve(input: String, _verbose: bool) -> (String, String) {
         results.push(n);
 
         let diffs = Vec::from_iter((1..prices.len()).map(|i| prices[i] - prices[i - 1]));
-        let mut seen = HashSet::new();
+        let mut seq = ((diffs[0] + 9) << 5) | ((diffs[1] + 9) << 10) | ((diffs[2] + 9) << 15);
+        seen.clear();
         for i in 3..diffs.len() {
-            let seq = (diffs[i - 3], diffs[i - 2], diffs[i - 1], diffs[i]);
+            seq = (seq >> 5) | (diffs[i] + 9) << 15;
             if !seen.contains(&seq) {
                 let current_price = prices[i + 1]; // diffs is offset by one
                 seen.insert(seq);
@@ -28,12 +31,11 @@ pub fn solve(input: String, _verbose: bool) -> (String, String) {
                     .entry(seq)
                     .and_modify(|bananas| *bananas += current_price)
                     .or_insert(current_price);
-            }
+            };
         }
     }
     let part1: i64 = results.iter().sum();
     let part2 = total_bananas.values().max().unwrap();
-    // 1995 is too low
 
     (part1.to_string(), part2.to_string())
 }
@@ -41,7 +43,6 @@ pub fn solve(input: String, _verbose: bool) -> (String, String) {
 #[test]
 fn test_part1() {
     let input = "1\n10\n100\n2024";
-    // let input = "123";
     assert_eq!(solve(input.to_string(), false).0, "37327623");
 }
 
